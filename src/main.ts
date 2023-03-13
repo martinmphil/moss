ba87a36cf0b71015a5773a0cb09bdea497994d11
Martin <martin.mphil@gmail.com>
2023-03-13 12:15:43 +0000

    application scaffold

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+// TEMP
+document.querySelector<HTMLDivElement>("#emailAddr")!.innerHTML = `
+  <div>
+<p>This is emailAddr div</p>
+  </div>
+`;
+
+// TEMP
+document.querySelector<HTMLDivElement>("#crux")!.innerHTML = `
+  <div>
+<p>This is crux div</p>
+  </div>
+`;
